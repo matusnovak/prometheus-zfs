825f0ec97da7de1f616dd3b0966d13907804ca09
Matus Novak <email@matusnovak.com>
2020-06-20 14:36:55 +0200

    Added metrics for zpool status

--- zfsprom.py
+++ zfsprom.py
@@ -5,7 +5,7 @@ import sys
 import re
 import subprocess
 import time
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server, Gauge, Enum
 
 
 def run(args: [str]):
@@ -15,6 +15,8 @@ def run(args: [str]):
     stdout, stderr = out.communicate()
 
     if out.returncode != 0:
+        if stdout:
+            print(stdout.decode("utf-8"))
         if stderr:
             print(stderr.decode("utf-8"))
         raise Exception('Command returned code {}'.format(out.returncode))
@@ -112,9 +114,44 @@ def getspace(metrics):
                 metrics['space_used'].labels(label).set(used)
 
 
+def status(metrics):
+    results = run(['zpool', 'status', '-c', 'serial,upath'])
+    header = False
+    levels = []
+    for result in results.split('\n'):
+        if result.strip().startswith('NAME'):
+            header = True
+            continue
+
+        if header:
+            tokens = result.split()
+            if len(tokens) >= 7:
+                metrics['status_state'].labels(
+                    source=tokens[0],
+                    serial=tokens[5],
+                    upath=tokens[6]
+                ).state(tokens[1])
+                metrics['status_read'].labels(
+                    source=tokens[0],
+                    serial=tokens[5],
+                    upath=tokens[6]
+                ).set(int(tokens[2]))
+                metrics['status_write'].labels(
+                    source=tokens[0],
+                    serial=tokens[5],
+                    upath=tokens[6]
+                ).set(int(tokens[3]))
+                metrics['status_cksum'].labels(
+                    source=tokens[0],
+                    serial=tokens[5],
+                    upath=tokens[6]
+                ).set(int(tokens[4]))
+
+
 def collect(metrics):
     iostat(metrics)
     getspace(metrics)
+    status(metrics)
 
 
 def main():
@@ -128,13 +165,16 @@ def main():
         'bw_write': Gauge('zfsprom_iostat_bw_write', 'Bandwidth write in MB', ['source']),
         'space_available': Gauge('zfsprom_space_available', 'Space available in GB', ['source']),
         'space_used': Gauge('zfsprom_space_used', 'Space used in GB', ['source']),
+        'status_state': Enum('zfsprom_status_state', 'Disk status', ['source', 'serial', 'upath'], states=['ONLINE', 'DEGRADED', 'FAULTED', 'OFFLINE', 'UNAVAIL', 'REMOVED']),
+        'status_read': Gauge('zfsprom_status_read', 'Read errors', ['source', 'serial', 'upath']),
+        'status_write': Gauge('zfsprom_status_write', 'Write errors', ['source', 'serial', 'upath']),
+        'status_cksum': Gauge('zfsprom_status_cksum', 'Checksum errors', ['source', 'serial', 'upath']),
     }
-    # collect(metrics)
 
     start_time = time.time()
     while True:
         elapsed_time = time.time() - start_time
-        if elapsed_time > 10.0:
+        if elapsed_time > 30.0:
             start_time = time.time()
             collect(metrics)
         time.sleep(0.1)
